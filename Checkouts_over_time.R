a3123201ee5baafe13c2336161ace4fe335cf013
clairelongcore <longcore@uw.edu>
2023-02-23 12:00:27 -0800

    linted

--- Checkouts_over_time.R
+++ Checkouts_over_time.R
@@ -5,29 +5,29 @@ library("ggplot2")
 
 #Sort for Agatha Christie books
 christie_overall <- spl_data %>%
-  filter(str_detect(Creator, "Christie") == T) %>%
-  filter(str_detect(Creator, "Agatha") == T) %>%
+  filter(str_detect(Creator, "Christie") == TRUE) %>%
+  filter(str_detect(Creator, "Agatha") == TRUE) %>%
   mutate(total_date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
 
 #Convert date to date format
-christie_overall$total_date <- as.Date(christie_overall$total_date, format = "%Y-%m-%d")
+christie_overall$total_date <- as.Date(christie_overall$total_date,
+                                       format = "%Y-%m-%d")
 
 christie_titles <- christie_overall %>%
-  
+
   #consolidate multiple editions
   mutate(Title = gsub(" /.*", "", christie_overall$Title)) %>%
-  
+
   #find only standard physical books
-  filter(str_detect(MaterialType, "^BOOK") == T) %>%
-  filter(str_detect(Subjects, "Large type") == F) %>%
-  
+  filter(str_detect(MaterialType, "^BOOK") == TRUE) %>%
+  filter(str_detect(Subjects, "Large type") == FALSE) %>%
+
   #Find four specific titles
-  filter(str_detect(Title, "Styles$") == T |
-         str_detect(Title, "Orient Express$") == T |
-         str_detect(Title, "none$") == T |
+  filter(str_detect(Title, "Styles$") == TRUE |
+         str_detect(Title, "Orient Express$") == TRUE |
+         str_detect(Title, "none$") == TRUE |
          str_detect(Title, "table$"))
-  
-  
+
 #Line graph of selected book checkouts over time
 ggplot(christie_titles) +
   geom_line(aes(x = total_date, y = Checkouts, color = Title)) +
@@ -36,4 +36,3 @@ ggplot(christie_titles) +
        x = "Month",
        y = "Number of Checkouts",
        color = "Book Title")
-  
