8c19cbc39785ea6e38fd12a120846c70a328e840
clairelongcore <longcore@uw.edu>
2023-02-23 12:00:41 -0800

    linted

--- Checkout_by_type.R
+++ Checkout_by_type.R
@@ -2,23 +2,22 @@
 library("dplyr")
 library("ggplot2")
 
-
 #Choose specific material types
 selected_types <- spl_data %>%
-  filter(MaterialType %in% c("BOOK", "AUDIOBOOK", "VIDEODISC", "EBOOK", "VISUAL", "ATLAS", "SOUNDDISC", "NOTATEDMUSIC"))
+  filter(MaterialType %in% c("BOOK", "AUDIOBOOK", "VIDEODISC", "EBOOK",
+                             "VISUAL", "ATLAS", "SOUNDDISC", "NOTATEDMUSIC"))
 
 #Calculate average checkouts per item by medium
 avg_checkout_by_type <- selected_types %>%
   group_by(MaterialType) %>%
-  summarize(by_type = mean(Checkouts, na.rm = T))
+  summarize(by_type = mean(Checkouts, na.rm = TRUE))
 
 #Bar chart of average checkouts per item by medium
 ggplot(avg_checkout_by_type) +
   geom_col(mapping = aes(x = by_type, y = MaterialType, fill = MaterialType)) +
   scale_color_brewer(palette = "Paired") +
   labs(title = "Average Checkouts By Item Type",
-       subtitle = "2022-Jan 2023", 
+       subtitle = "2022-Jan 2023",
        x = "Average Checkouts",
-       y = "Type of Material", 
+       y = "Type of Material",
        fill = "Type of Material")
-
